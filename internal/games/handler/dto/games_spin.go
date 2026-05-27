302174fd85426876ffc046392766fecc6056106e
tanawin.jen <tanawin.jen@codelabdev.co>
2026-05-27 23:50:06 +0700

    feat(games): add Spin endpoint and request validation for game spinning functionality

---
+++ internal/games/handler/dto/games_spin.go
@@ -0,0 +1,22 @@
+package dto
+
+import (
+	"gamification-app/internal/games/domain"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type SpinRequest struct {
+	PointsEarned int `json:"points_earned" validate:"required,min=1"`
+}
+
+func (r *SpinRequest) Validate(v *validator.Validate) error {
+	return v.Struct(r)
+}
+
+func (r *SpinRequest) ToDomainInput(userID uint) domain.SpinInput {
+	return domain.SpinInput{
+		UserID:       userID,
+		PointsEarned: r.PointsEarned,
+	}
+}
