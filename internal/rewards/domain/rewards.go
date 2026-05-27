b8ddd83a8a474ec81b997f24c7e52c41638fe8b1
tanawin.jen <tanawin.jen@codelabdev.co>
2026-05-27 22:04:13 +0700

    feat(rewards): add domain models and repository/service interfaces for rewards functionality

---
+++ internal/rewards/domain/rewards.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"errors"
+	"gamification-app/pkg/constants"
+	"time"
+)
+
+type ClaimRewardInput struct {
+	UserID   uint
+	RewardID uint
+}
+
+type Rewards struct {
+	ID         uint
+	Name       string
+	CheckPoint int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+type FindAllRewardsRequest struct {
+	Search    string
+	Page      int
+	Limit     int
+	SortOrder constants.SortOrder
+}
+
+type FindAllRewardsResult struct {
+	Items []Rewards
+	Total int64
+}
+
+var (
+	ErrRewardNotFound       = errors.New("reward not found")
+	ErrInsufficientPoints   = errors.New("insufficient points")
+	ErrRewardAlreadyClaimed = errors.New("reward already claimed")
+)
